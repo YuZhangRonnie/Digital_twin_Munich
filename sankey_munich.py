ff66c7704321e6a42ed21909a575c18aeb9d40bb
RonnieZHANG <42303571+YuZhangRonnie@users.noreply.github.com>
2020-12-10 15:25:52 +0000

    Update sankey_munich.py

--- sankey_munich.py
+++ sankey_munich.py
@@ -1,43 +1,49 @@
 #In[]
 import plotly.graph_objects as go
 import urllib, json
+import pandas as pd
+import chart_studio
+import chart_studio.plotly as py
 
-url = 'https://raw.githubusercontent.com/plotly/plotly.js/master/test/image/mocks/sankey_energy.json'
-response = urllib.request.urlopen(url)
-data = json.loads(response.read())
-
-# override gray link colors with 'source' colors
-opacity = 0.4
-# change 'magenta' to its 'rgba' value to add opacity
-data['data'][0]['node']['color'] = ['rgba(255,0,255, 0.8)' if color == "magenta" else color for color in data['data'][0]['node']['color']]
-data['data'][0]['link']['color'] = [data['data'][0]['node']['color'][src].replace("0.8", str(opacity))
-                                    for src in data['data'][0]['link']['source']]
-
+# %%
+df_flows = pd.read_excel('energy_sankey.xlsx', engine = 'openpyxl',sheet_name = 'flows')
+df_labels= pd.read_excel('energy_sankey.xlsx', engine = 'openpyxl',sheet_name = 'labels')
+df_flows = df_flows.dropna()
+df_labels = df_labels.dropna()
+df_labels.head()
+# %%
+opacity = 0.3
 fig = go.Figure(data=[go.Sankey(
     valueformat = ".0f",
-    valuesuffix = "TWh",
+    valuesuffix = "MJ",
+    arrangement = 'freeform',
     # Define nodes
     node = dict(
       pad = 15,
       thickness = 15,
-      line = dict(color = "black", width = 0.5),
-      label =  data['data'][0]['node']['label'],
-      color =  data['data'][0]['node']['color']
+      line = dict(color = "black", width = 1.5),
+      label =  df_labels.node_label,
+      color =  df_labels.node_color
     ),
     # Add links
     link = dict(
-      source =  data['data'][0]['link']['source'],
-      target =  data['data'][0]['link']['target'],
-      value =  data['data'][0]['link']['value'],
-      label =  data['data'][0]['link']['label'],
-      color =  data['data'][0]['link']['color']
+      source =  df_flows.Source,
+      target =  df_flows.Target,
+      value =  df_flows.Value,
+      #color =  df_flows.Color
 ))])
 
-fig.update_layout(title_text="Example Digital Twin of Munich",
+fig.update_layout(title_text="Digital Twin of Munich - Energy flows",
                   font_size=10)
 fig.show()
 # %%
-data['data'][0]['link']['source']
+username = 'RonnieZHANG' # your username
+api_key = 'kyTNcDpogBqGdpF2Dh9g' # your api key - go to profile > settings > regenerate key
+chart_studio.tools.set_credentials_file(username=username, api_key=api_key)
+
 # %%
-data['data'][0]['node']
+py.plot(fig, filename = 'sankey_energy', auto_open=True)
+# %%
+import chart_studio.tools as tls
+tls.get_embed('https://plot.ly/~Ronniezhang/1/#/') #change to your url
 # %%
